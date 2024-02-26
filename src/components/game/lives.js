c416d882e871a1798af82dd6feffd04de1ee2da4
imrankhan8107 <imran.pathan.8202@gmail.com>
2024-02-26 22:08:59 +0530

    update

--- src/components/game/lives.js
+++ src/components/game/lives.js
@@ -3,19 +3,17 @@ import React from "react";
 
 export default function Lives({ livesLeft }) {
   return (
-    <div>
-      <div className="hangman-container">
-        <div className="pole"></div>
-        <div className={`hangman hangman-${6 - livesLeft}`}>
-          <div className="hangman__element"></div>
-          <div className="hangman__element"></div>
-          <div className="hangman__element"></div>
-          <div className="hangman__element"></div>
-          <div className="hangman__element"></div>
-          <div className="hangman__element"></div>
-        </div>
-        <div className="lives">Lives: {livesLeft}</div>
+    <div className="hangman-container">
+      <div className="pole"></div>
+      <div className={`hangman hangman-${6 - livesLeft}`}>
+        <div className="hangman__element"></div>
+        <div className="hangman__element"></div>
+        <div className="hangman__element"></div>
+        <div className="hangman__element"></div>
+        <div className="hangman__element"></div>
+        <div className="hangman__element"></div>
       </div>
+      <div className="lives">Lives: {livesLeft}</div>
     </div>
   );
 }
