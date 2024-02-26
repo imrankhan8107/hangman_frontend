89caa292e405e8118a04c9cefb78e42405452d1e
imrankhan8107 <imran.pathan.8202@gmail.com>
2024-02-26 23:21:51 +0530

    update

--- src/api/sessions.js
+++ src/api/sessions.js
@@ -1,8 +1,8 @@
-const BASE_URL = "https://hangman-backend-t6nf.onrender.com/api/sessions";
+// const BASE_URL = "https://hangman-backend-t6nf.onrender.com/api/sessions";
 // const BASE_URL =
 //   "https://hangman-service-8202-abe81026f0be.herokuapp.com/api/sessions";
 
-// const BASE_URL = "http://localhost:8000/api/sessions";
+const BASE_URL = "http://localhost:8000/api/sessions";
 
 export async function createSession(name) {
   const response = await fetch(BASE_URL, {
